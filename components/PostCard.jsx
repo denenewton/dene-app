65b9050ea4e6aee129b64f618e6358e1b6de7425
Daniel Santos <denenewton@icloud.com>
2024-02-07 03:44:40 -0300

    fixes

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -15,9 +15,9 @@ const PostCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const handleProfileClick = () => {
     console.log(post);
 
-    if (post.creator._id === session?.user.id) return router.push("/profile");
+    if (post.creator?._id === session?.user.id) return router.push("/profile");
 
-    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+    router.push(`/profile/${post.creator?._id}?name=${post.creator?.username}`);
   };
 
   const handleCopy = () => {
@@ -34,7 +34,7 @@ const PostCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
           onClick={handleProfileClick}
         >
           <Image
-            src={post.creator.image}
+            src={post?.creator?.image}
             alt='user_image'
             width={40}
             height={40}
@@ -43,10 +43,10 @@ const PostCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
 
           <div className='flex flex-col'>
             <h3 className='font-satoshi font-semibold text-gray-900'>
-              {post.creator.username}
+              {post?.creator?.username}
             </h3>
             <p className='font-inter text-sm text-gray-500'>
-              {post.creator.email}
+              {post?.creator?.email}
             </p>
           </div>
         </div>
@@ -54,18 +54,18 @@ const PostCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
         <div className='copy_btn' onClick={handleCopy}>
           <Image
             src={
-              copied === post.body
+              copied === post?.body
                 ? "/assets/icons/tick.svg"
                 : "/assets/icons/copy.svg"
             }
-            alt={copied === post.body ? "tick_icon" : "copy_icon"}
+            alt={copied === post?.body ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
           />
         </div>
       </div>
-      <p className='my-4 font-satoshi text-md text-gray-700'>{post.title}</p>
-      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.body}</p>
+      <p className='my-4 font-satoshi text-md text-gray-700'>{post?.title}</p>
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post?.body}</p>
       <p
         className='font-inter text-sm blue_gradient cursor-pointer'
         onClick={() => handleTagClick && handleTagClick(post.tag)}
@@ -73,7 +73,7 @@ const PostCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
         #{post.tag}
       </p>
 
-      {session?.user.id === post.creator._id && pathName === "/profile" && (
+      {session?.user.id === post.creator?._id && pathName === "/profile" && (
         <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
           <p
             className='font-inter text-sm green_gradient cursor-pointer'
